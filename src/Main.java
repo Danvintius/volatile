20de23899fabb0374b30e7aa78e27af586cb8ef6
Danvintius <danila.94@mail.ru>
2023-10-19 20:21:30 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,32 @@ public class Main {
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
+        System.out.println(generateText());
+
+        new Thread(() -> {
+            int counter1 = 0;
+            for (int i = 0; i < texts.length; i++) {
+                if (texts[i] == texts[i].reverse()) {
+                    
+                }
+            }
+        });
+
+        new Thread(() -> {
+            int counter2 = 0;
+            for (int i = 0; i < texts.length; i++) {
+                if (texts[i] == "a") {
+
+                }
+            }
+        });
+
+        new Thread(() -> {
+            int counter3 = 0;
+            for (int i = 0; i < texts.length; i++) {
+
+            }
+        });
     }
 
     public static String generateText(String letters, int length) {
