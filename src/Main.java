87c300ba1ef4b1319acc757f7e4c5fd0964e2181
Danvintius <119405034+Danvintius@users.noreply.github.com>
2024-02-16 13:16:37 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,50 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         String[] texts = new String[100_000];
+        AtomicInteger counter3 = new AtomicInteger();
+        AtomicInteger counter4 = new AtomicInteger();
+        AtomicInteger counter5 = new AtomicInteger();
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
-        System.out.println(generateText());
 
-        new Thread(() -> {
-            int counter1 = 0;
-            for (int i = 0; i < texts.length; i++) {
-                if (texts[i] == texts[i].reverse()) {
-                    
+        Thread palindrome = new Thread(() -> {
+            for (String text : texts) {
+                if (isPalindrome(text) && !isSameChar(text)) {
+                    incrementCounter(text.length());
                 }
             }
         });
+        palindrome.start();
 
-        new Thread(() -> {
-            int counter2 = 0;
-            for (int i = 0; i < texts.length; i++) {
-                if (texts[i] == "a") {
-
+        Thread sameChar = new Thread(() -> {
+            for (String text : texts) {
+                if (isSameChar(text)) {
+                    incrementCounter(text.length());
                 }
             }
         });
+        sameChar.start();
 
-        new Thread(() -> {
-            int counter3 = 0;
-            for (int i = 0; i < texts.length; i++) {
-
+        Thread ascendingOrder = new Thread(() -> {
+            for (String text : texts) {
+                if (!isPalindrome(text) && isAscendingOrder(text)) {
+                    incrementCounter(text.length());
+                }
             }
         });
+        ascendingOrder.start();
+
+        sameChar.join();
+        ascendingOrder.join();
+        palindrome.join();
+
+        System.out.println("Красивых слов с длиной 3: " + counter3);
+        System.out.println("Красивых слов с длиной 4: " + counter4);
+        System.out.println("Красивых слов с длиной 5: " + counter5);
     }
 
     public static String generateText(String letters, int length) {
@@ -43,4 +55,35 @@ public class Main {
         }
         return text.toString();
     }
+
+    public static boolean isPalindrome(String text) {
+        return text.equals(new StringBuilder(text).reverse().toString());
+    }
+
+    public static boolean isSameChar(String text) {
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i) != text.charAt(i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean isAscendingOrder(String text) {
+        for (int i = 1; i < text.length(); i++) {
+            if (text.charAt(i) < text.charAt(i - 1))
+                return false;
+        }
+        return true;
+    }
+
+    public static void incrementCounter(int textLength) {
+        if (textLength == 3) {
+            counter3.getAndIncrement();
+        } else if (textLength == 4) {
+            counter4.getAndIncrement();
+        } else {
+            counter5.getAndIncrement();
+        }
+    }
+
 }
